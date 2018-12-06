edb42d188b1f7b6bcfb4a9afd941b0779e250a63
Zack <Zack@DESKTOP-CMRQ39K>
2018-12-06 16:24:20 +0800

    测试

--- src/com/hadoop/test/feilsss.java
+++ src/com/hadoop/test/feilsss.java
@@ -9,5 +9,7 @@ public class feilsss {
         File directory = new File("");// ²ÎÊýÎª¿Õ
         String courseFile = directory.getCanonicalPath();
         System.out.println(courseFile);
+        
+        
 	}
 }
